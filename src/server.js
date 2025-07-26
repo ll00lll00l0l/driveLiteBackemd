9bbdb0bd6de06eef4380c0a9f35f10570e1c5219
hazan <ll00lll00l0l@gmail.com>
2025-07-27 01:51:11 +0530

    add cilent Url

--- src/server.js
+++ src/server.js
@@ -12,7 +12,7 @@ const app = express();
 const port = process.env.PORT || 5000;
 
 app.use(cors({
-  origin: ["http://localhost:3000"],
+  origin: ["http://localhost:3000",`${process.env.CLIENT_URL}`],
   methods: "GET,POST,PUT,DELETE",
   credentials: true
 }));
