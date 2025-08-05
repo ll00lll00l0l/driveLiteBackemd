e740d4355dc696ea32c19298c395d26f46b59f69
hazan <ll00lll00l0l@gmail.com>
2025-08-05 23:24:26 +0530

    fix cros error

--- src/server.js
+++ src/server.js
@@ -1,10 +1,10 @@
 const express = require("express");
 const cors = require("cors");
-const bodyParser = require('body-parser');
-const path = require('path');
+const bodyParser = require("body-parser");
+const path = require("path");
 
-const connectDB = require('./db/conn');
-const appRoutes = require('./routes');
+const connectDB = require("./db/conn");
+const appRoutes = require("./routes");
 const { uploadImage } = require("./controllers/mediaController.js");
 const router = require("./routes");
 const upload = require("./middleware/uploadMiddleware");
@@ -15,16 +15,18 @@ require("dotenv").config({ path: ".env" });
 const app = express();
 const port = process.env.PORT || 5000;
 
-app.use(cors({
-  origin: [`http://localhost:3000,${process.env.CLIENT_URL}`],
-  methods: "GET,POST,PUT,DELETE",
-  credentials: true
-}));
+app.use(
+  cors({
+    origin: [`http://localhost:3000`, process.env.CLIENT_URL],
+    methods: "GET,POST,PUT,DELETE",
+    credentials: true,
+  })
+);
 
 app.use(express.json());
 app.use(bodyParser.json());
-app.use('/media', express.static(path.join(__dirname, 'media')));
-app.use('/', appRoutes);
+app.use("/media", express.static(path.join(__dirname, "media")));
+app.use("/", appRoutes);
 router.post("/api/upload", upload.single("image"), uploadImage);
 router.get("/api/resize", resizeImage);
 
